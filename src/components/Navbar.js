2241038b99f99ccfbe380c26da3f0650d72ca171
Sanchita Kapur <81643779+kapursanchita@users.noreply.github.com>
2023-09-07 13:16:30 +0530

    Bug Fixes and UI Changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-success shadow">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">
+          <i className="bi bi-whatsapp"></i> WhatsApp Connect
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
